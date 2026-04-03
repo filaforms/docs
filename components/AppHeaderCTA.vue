304b74119ddeb6f1547b5ece6e63f145142a91c8
manukminasyan <manuk.minasyan1@gmail.com>
2026-04-03 11:34:25 +0400

    fix: remove target blank from header filaforms.app link

--- components/AppHeaderCTA.vue
+++ components/AppHeaderCTA.vue
@@ -1,7 +1,6 @@
 <template>
     <NuxtLink
         to="https://filaforms.app"
-        target="_blank"
         class="text-sm text-[var(--ui-text-muted)] hover:text-[var(--ui-text)] transition-colors"
     >
         filaforms.app
